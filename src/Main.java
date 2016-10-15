ea03b1806cc9b1c0aac1f82570bc0458892918d4
alejo <alejo@10.31.29.202>
2016-10-14 20:21:29 -0400

    Bug fixes: flag

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
-import javax.swing.JButton;
+
 import javax.swing.JFrame;
 
 public class Main {
 	public static void main(String[] args) {
 		
 		JFrame myFrame = new JFrame("Mine Sweeper");
-		JButton refresh = new JButton("Refresh");
+		
 		
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(400, 150);
 		myFrame.setSize(400, 400);
 		MyGrid myPanel = new MyGrid();
 		myFrame.add(myPanel);
-		myPanel.add(refresh);
+		
 
 		Mouse myMouseAdapter = new Mouse();
 		myFrame.addMouseListener(myMouseAdapter);
